eeb135ca05bc276be0b5b0420360a716a667a4fc
Lilia Amundsen <lilia.amundsen@gmail.com>
2021-02-04 19:37:36 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+  var rotation = 0, 
+    scrollLoc = $(document).scrollTop();
+  $(window).scroll(function() {
+    var newLoc = $(document).scrollTop();
+    var diff = newLoc -  scrollLoc;
+    rotation += diff, scrollLoc = newLoc;
+    var rotationStr = "rotate(" + rotation + "deg)";
+    $("#logo").css({
+      "-webkit-transform": rotationStr,
+      "-moz-transform": rotationStr,
+      "transform": rotationStr
+    });
+  });
+});
+
+$(document).ready(function(){
+   	$("#button").click(function(){
+        $("#contact").animate({
+            height: 'toggle'
+        });
+    });
+});
---
+++ visuals/script/p5js_script.js
@@ -0,0 +1,107 @@
+// Sketch One
+var c1 = function(s){
+s.t;
+s.i;
+s.ang;
+s.rad;
+s.x;
+s.y;
+
+s.setup = function() {
+  s.createCanvas(1200, 600);
+  s.background("black");
+  s.stroke(255, 20); //stroke(rgb, alpha= opacity of the stroke
+  s.noFill();
+  s.t = 0;
+}
+
+s.draw = function() {
+  s.translate(s.width/2, s.height/2);
+  s.beginShape();
+  for (s.i = 0; s.i < s.random(10,700); s.i++) {
+    s.ang = s.map(s.i, 0, 100, 0.5, s.TWO_PI);
+    s.rad = 600 * s.noise(s.i * 0.07, s.t * 0.008);
+    s.x = s.rad * s.sin(s.ang)-3.5;
+    s.y = s.rad * 0.6;
+    s.curveVertex(s.x, s.y);
+  }
+  s.endShape(s.CLOSE);
+
+  s.t += 0.3; //time
+
+ // reset after 1k frames 
+ // idea for later: change color 
+
+  if (s.frameCount %1000 == 0) {
+  s.background("black");
+  s.stroke(255, 20);
+}  
+}
+}
+
+var myp5 = new p5(c1, "c1");
+
+
+
+
+
+// Sketch Two
+var c2 = function(p) { 
+
+p.nVal; // noise value
+p.nInt // noise intensity
+p.nAmp  // noise amplitude
+p.resolution = 500; // how many points in the circle
+p.rad = 150;
+p.x;
+p.y;
+p.u;
+p.a;
+p.i;
+
+p.t = 0; // time passed
+p.tChange = 0.0010; // how quick time flies
+
+p.setup = function() {
+  p.createCanvas(750, 500 );
+  p.noiseDetail(5);
+  p.noFill();
+  p.stroke("#f063e9");
+  p.strokeWeight(1);
+}
+
+p.drawRings = function() {
+  p.beginShape();
+  for (p.a=0; p.a<=p.TWO_PI; p.a+=p.TWO_PI/p.resolution) {
+
+    p.nVal = p.map(p.noise(p.cos(p.a)*p.nInt+1, p.sin(p.a)*p.nInt+1, p.t ), 0, 0.10, p.nAmp, p.u, false); // map noise value to match the amplitude
+
+    p.x = p.cos(p.a)*p.rad *p.nVal;
+    p.y = p.sin(p.a)*p.rad *p.nVal;
+
+    p.vertex(p.x, p.y);
+    
+    };
+  p.endShape(p.CLOSE);
+};
+
+p.draw = function(){
+	p.background("navy");
+  p.translate(p.width/2, p.height/2);
+  
+  p.nInt = p.map(0, 0, p.width, 0.5, 10); // intensity
+  p.nAmp = p.map(0, 0.8, p.height, 0.007, 0.10); // amplitude
+  p.t = p.t + p.tChange;
+  p.u=0.04;
+
+for (p.i=0; p.i <= 70; p.i+=1) {
+  p.u +=0.035;
+  p.drawRings(p.u);
+};
+};
+};
+
+myp5 = new p5(c2, "c2");
+
+
+
